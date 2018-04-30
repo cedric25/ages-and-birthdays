e972b2aff2b63f28cce7de746a7dce343700519d
Cedric Nicoloso <cnicoloso@octo.com>
2018-04-30 21:55:50 +1000

    Back to an array for list of persons...

--- store/index.js
+++ store/index.js
@@ -3,33 +3,29 @@ import Vuex from 'vuex'
 /**
  * Example of 'importantPersons':
  * {
- *   1: {
+ *   [
  *     id: 1,
  *     name: 'Cedric',
- *   }
+ *   ]
  * }
  */
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      importantPersons: {},
+      importantPersons: [],
     },
     mutations: {
       addNewImportantPerson(state, newPerson) {
-        const newPersonIndex = Object.keys(state.importantPersons).length
+        const newPersonIndex = state.importantPersons.length
         const newPersonWithId = Object.assign({}, newPerson, { id: newPersonIndex })
-        state.importantPersons = { ...state.importantPersons, [newPersonIndex]: newPersonWithId }
+        state.importantPersons.push(newPersonWithId)
       },
       updatePerson(state, updatedPerson) {
-        const nwPersonsList = { ...state.importantPersons }
-        nwPersonsList[updatedPerson.id].name = updatedPerson.name
-        // state.importantPersons[updatedPerson.id].name = updatedPerson.name
-        // Vue.set(state.importantPersons[0], 'name', updatedPerson.name)
-        state.importantPersons = nwPersonsList
+        state.importantPersons[updatedPerson.id].name = updatedPerson.name
       },
       removeAllPersons(state) {
-        state.importantPersons = {}
+        state.importantPersons = []
       },
     },
     getters: {
