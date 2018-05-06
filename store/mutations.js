685aa4225fe7e006067756b099def5c198435655
Cedric Nicoloso <cnicoloso@octo.com>
2018-05-06 21:12:21 +1000

    Safer implementation of updatePerson()

--- store/mutations.js
+++ store/mutations.js
@@ -6,10 +6,21 @@ export default {
     state.importantPersons.push(newPerson)
   },
 
-  updatePerson(state, updatedPerson) {
-    const personToUpdate = state.importantPersons.find(person => person.id = updatedPerson.id)
-    personToUpdate.name = updatedPerson.name
-    personToUpdate.birthday = updatedPerson.birthday
+  updatePerson(state, newInfo) {
+
+    const personToUpdate = state.importantPersons.find(person => person.id === newInfo.id)
+
+    const updatedPerson = Object.assign({}, personToUpdate,
+      { name: newInfo.name },
+      { birthday: newInfo.birthday }
+    )
+
+    let newPersonsList = state.importantPersons.filter(person => {
+      return person.id !== newInfo.id
+    })
+    newPersonsList.push(updatedPerson)
+
+    state.importantPersons = newPersonsList
   },
 
   deletePerson(state, personToDelete) {
