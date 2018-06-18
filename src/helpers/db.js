65a4abaaf8929afbf887cf0b2110ecdc5b981363
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2018-06-18 22:44:08 +0200

    Remove unused function

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,21 +1,5 @@
 import * as firebase from 'firebase'
 
-export function addImportantPerson(userId, newPerson) {
-
-  // Get a key for a new Person
-  const newPersonKey = firebase.database().ref().child(`users/${userId}/importantPersons`).push().key
-
-  firebase.database().ref().update({
-    [`users/${userId}/importantPersons/${newPersonKey}`]: newPerson
-  }, err => {
-    if (err) {
-      console.error('Sync newPerson into Firebase', err)
-    } else {
-      console.log('Sync newPerson into Firebase done!')
-    }
-  })
-}
-
 export function setImportantPersons(userId, importantPersons) {
   firebase.database().ref().update({
     [`users/${userId}/importantPersons`]: importantPersons
