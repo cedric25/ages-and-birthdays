d7757f23401b6b11ee45c2a4f97c02a93d74ff1d
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-04-16 00:38:12 +0200

    Some PWA improvements with icons

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,10 @@ module.exports = {
   ],
   publicPath: process.env.NODE_ENV === 'production'
     ? '/ages-and-birthdays/'
-    : '/'
+    : '/',
+  pwa: {
+    workboxPluginMode: 'GenerateSW',
+    name: 'Birthdays',
+    themeColor: '#1976D2',
+  },
 }
