3c7e5ed2b772bb0512fdfc48ac43fc2d1da71c89
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-04-29 12:37:15 +0200

    Update Firebase config for analytics

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,6 +3,7 @@ export default {
   authDomain: 'ages-and-birthdays.firebaseapp.com',
   databaseURL: 'https://ages-and-birthdays.firebaseio.com',
   projectId: 'ages-and-birthdays',
-  storageBucket: '',
-  messagingSenderId: '302369470244'
+  storageBucket: "ages-and-birthdays.appspot.com",
+  messagingSenderId: "302369470244",
+  appId: "1:302369470244:web:136c4bce61b1af702ae5b1"
 }
