e4d2f81b2e536b90db218e3ede881e0548a5fa2a
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-05-09 22:11:24 +0200

    Switch to more flexible InjectManifest pwa sw mode

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   pwa: {
-    workboxPluginMode: 'GenerateSW',
     name: 'Birthdays',
     themeColor: '#1976D2',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // https://cli.vuejs.org/core-plugins/pwa.html#configuration
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/service-worker.js',
+      // ...other Workbox options...
+    },
   },
 }
