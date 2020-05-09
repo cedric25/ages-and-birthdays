4cbfa0ac86063fe79131e6963b86547bbd932b06
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-05-09 22:12:09 +0200

    Ask for notif permission and send test notif

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,47 @@
     components: {
       TopMenu,
     },
+    mounted() {
+      console.log('mounted, Notification.permission:', Notification.permission)
+      if (Notification.permission !== 'granted') {
+        Notification.requestPermission(function (status) {
+          console.log('Notification permission status:', status)
+
+          setTimeout(() => {
+            this.displayNotification()
+          })
+        })
+      } else {
+        this.displayNotification()
+      }
+    },
+    methods: {
+      displayNotification() {
+        if (Notification.permission === 'granted') {
+          navigator.serviceWorker.getRegistration().then(function (reg) {
+            console.log('here', reg)
+            var options = {
+              // body: 'Here is a notification body!',
+              icon: 'favicon.ico',
+              vibrate: [100, 50, 100],
+              data: {
+                dateOfArrival: Date.now(),
+                primaryKey: 1,
+              },
+              actions: [
+                {
+                  action: 'explore',
+                  title: 'Go to app',
+                  icon: 'images/checkmark.png',
+                },
+                { action: 'close', title: 'Close notification', icon: 'images/xmark.png' },
+              ],
+            }
+            reg.showNotification('Tommy is turning 23 today!', options)
+          })
+        }
+      },
+    },
   }
 </script>
 
