7a0988431828de8e82f4f4c0ffedb45473f6ca3a
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2021-01-19 22:06:33 +0100

    Setup tailwind :art: :rocket:

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <!-- use the router-link component for navigation. -->
     <!-- specify the link by passing the `to` prop. -->
     <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-    <router-link to="/">Go to Home</router-link>
+    <router-link to="/" class="mr-2">Go to Home</router-link>
     <router-link to="/about">Go to About</router-link>
   </p>
   <router-view></router-view>
@@ -14,14 +14,3 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
