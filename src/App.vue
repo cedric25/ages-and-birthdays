95f9587aaa03b470b80d8bacb4eab50c929e7839
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2021-01-19 21:58:11 +0100

    Set up router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <p>
+    <!-- use the router-link component for navigation. -->
+    <!-- specify the link by passing the `to` prop. -->
+    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/about">Go to About</router-link>
+  </p>
+  <router-view></router-view>
 </template>
 
 <script setup>
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+]
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
