b5899fa530f8901b873d936db6c046bc81ae7ae5
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-06-25 22:20:26 +0200

    A Firebase user without a list of important persons is valid

--- src/services/firebase/checkDbUserData.ts
+++ src/services/firebase/checkDbUserData.ts
@@ -2,5 +2,9 @@ export function checkDbUserData(userData: any) {
   if (!userData) {
     return false
   }
-  return userData.user && userData.importantPersons && userData.groups
+  return (
+    userData.user &&
+    // && userData.importantPersons
+    userData.groups
+  )
 }
